915999e6276f3ece86446e85b457a909f83e824e
Thomas <thomas.friedl@formulastudent-regensburg.de>
2016-11-06 14:12:41 +0100

    tested

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,24 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'controller' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "SKEAZ1284.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_USB_Device_0                /* USB Device 0 */
+#define RTE_USB_Host_0                  /* USB Host 0 */
+
+#endif /* RTE_COMPONENTS_H */
